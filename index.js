a2b3e8378c6323a35e8e3d2621d0e29458629aaf
Manuel  Silveyra <silveyra@us.ibm.com>
2017-01-18 01:46:03 +0000

    req.ip change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var express = require('express');
 
 var app = express();
 
-console.log(req);
+console.log("req.ip =" + req.ip);
 
 // Configure express with the settings found in
 // our config.js file
